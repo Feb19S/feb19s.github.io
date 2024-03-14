2a58acecf0332f8a1718529021b7f3887c763b83
Blake <blakeshi@vland.cc>
2024-03-14 20:08:56 +0800

    Site updated: 2024-03-14 20:08:55

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
+/*
+ * @Author: Blake blakeshi@vland.cc
+ * @Date: 2024-03-14
+ * @LastEditors: Blake blakeshi@vland.cc
+ * @LastEditTime: 2024-03-14
+ * @FilePath: \hexo_blog\themes\hollow\source\sw.js
+ * @Description: 
+ */
 "use strict";
 (function() {
   var cacheVersion = "-180503";
   var staticCacheName = "asset" + cacheVersion;
   var maxEntries = 100;
-  self.importScripts("https://cdn.jsdelivr.net/npm/sw-toolbox@3.6.0/sw-toolbox.js");
+  self.importScripts("https://cdn.bytedance.com/npm/sw-toolbox@3.6.0/sw-toolbox.js");
 
   self.toolbox.options.debug = false;
   self.toolbox.options.networkTimeoutSeconds = 1;
